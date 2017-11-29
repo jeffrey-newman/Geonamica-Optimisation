4e0d5ae1d0075b0d635714c4cf0127486e867779
jeffrey-newman <jeffrey-newman@users.noreply.github.com>
2017-11-29 13:46:32 +1030

    SaveMaps help now documents legend file format; test for directories with qt dialog on problems

--- GUI/PathifyQt.cpp
+++ GUI/PathifyQt.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "PathifyQt.hpp"
+
+#include <QMessageBox>
+#include <iostream>
+
+bool
+pathifyQMsg(CmdLinePaths & path)
+{
+    path.second = boost::filesystem::path(path.first);
+    if (!(boost::filesystem::exists(path.second)))
+    {
+        QMessageBox msgBox;
+        msgBox.setText("Warning: path \" << path.first << \" does not exist");
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        int ret = msgBox.exec();
+        return false;
+    }
+    return true;
+}
+
+bool
+pathifyMkQMsg(CmdLinePaths & path)
+{
+
+    path.second = boost::filesystem::path(path.first);
+    if (!(boost::filesystem::exists(path.second)))
+    {
+        try
+        {
+            boost::filesystem::create_directories(path.second);
+            std::cout << "path " << path.first << " did not exist, so created\n";
+        }
+        catch(boost::filesystem::filesystem_error& e)
+        {
+            QMessageBox msgBox;
+            msgBox.setText("Attempted to create \" << path.first << \" but was unable");
+            msgBox.setStandardButtons(QMessageBox::Ok);
+            msgBox.setStandardButtons(QMessageBox::Ok);
+            int ret = msgBox.exec();
+            return false;
+        }
+
+        return true;
+    }
+    return true;
+
+}
--- GUI/PathifyQt.hpp
+++ GUI/PathifyQt.hpp
@@ -5,11 +5,12 @@
 #ifndef GEON_OPT_PATHIFYQT_HPP
 #define GEON_OPT_PATHIFYQT_HPP
 
+#include "../Pathify.hpp"
 
-class PathifyQt
-{
+bool
+pathifyQMsg(CmdLinePaths & path);
 
-};
-
+bool
+pathifyMkQMsg(CmdLinePaths & path);
 
 #endif //GEON_OPT_PATHIFYQT_HPP
